40cd14da1bc880c23641ce9ba5b22259fc9b86b4
Sebastian Montoya <semontoya@unal.edu.co>
2023-03-03 21:28:33 -0500

    Gitignore y Header basic config v2
    Finalización de código cofiguración básica

--- HAL_start/Inc/stm32f411xx_hal.h
+++ HAL_start/Inc/stm32f411xx_hal.h
@@ -176,6 +176,44 @@ typedef struct{
 #define GPIO_PUPDR_RESERVED		3
 
 /*Definicion de los nombres de los pines*/
+#define PIN_0		0
+#define PIN_1		1
+#define PIN_2		2
+#define PIN_3		3
+#define PIN_4		4
+#define PIN_5		5
+#define PIN_6		6
+#define PIN_7		7
+#define PIN_8		8
+#define PIN_9		9
+#define PIN_10		10
+#define PIN_11		11
+#define PIN_12		12
+#define PIN_13		13
+#define PIN_14		14
+#define PIN_15		15
+
+/*Definicion de funciones alternativas*/
+#define AF0			0b0000
+#define AF1			0b0001
+#define AF2			0b0010
+#define AF3			0b0011
+#define AF4			0b0100
+#define AF5			0b0101
+#define AF6			0b0110
+#define AF7			0b0111
+#define AF8			0b1000
+#define AF9			0b1001
+#define AF10		0b1010
+#define AF11		0b1011
+#define AF12		0b1100
+#define AF13		0b1101
+#define AF14		0b1110
+#define AF15		0b1111
+
+
+
+
 
 
 
