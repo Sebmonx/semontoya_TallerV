20f41d44554d7069b6345c672c65969c3b84eb43
Semontoya <semontoya@unal.edu.co>
2023-04-09 00:50:06 -0500

    Avance tarea
    Configuracion adecuada de interrupciones externas

--- f/Src/leds.c
+++ f/Src/leds.c
@@ -17,13 +17,13 @@
 	BasicTimer_Handler_t timerDisplay = {0};
 
 	/* Handlers de GPIO display */
-	GPIO_Handler_t handPA8 = {0};
-	GPIO_Handler_t handPB10 = {0};
-	GPIO_Handler_t handPC4 = {0};
-	GPIO_Handler_t handPB5 = {0};
-	GPIO_Handler_t handPB3 = {0};
-	GPIO_Handler_t handPA7 = {0};
-	GPIO_Handler_t handPA9 = {0};
+	GPIO_Handler_t bit0 = {0};
+	GPIO_Handler_t bit1 = {0};
+	GPIO_Handler_t bit2 = {0};
+	GPIO_Handler_t bit3 = {0};
+	GPIO_Handler_t bit4 = {0};
+	GPIO_Handler_t bit5 = {0};
+	GPIO_Handler_t bit6 = {0};
 
 	/* Arreglo para encendido de LEDs 7 segmentos */
 	GPIO_Handler_t bits_7Seg[7] = {0};
@@ -35,7 +35,7 @@
 	GPIO_Handler_t tranDec = {0};
 	GPIO_Handler_t tranUni = {0};
 
-/* Variables main */
+/* Variables genericas */
 	BasicTimer_Handler_t timerLED2 = {0};
 	GPIO_Handler_t handLED2 = {0};
 	uint8_t numero = 0;
@@ -45,20 +45,19 @@
 	uint8_t modo = 0;
 
 /* Variables Encoder */
-	EXTI_Config_t encodCLK = {0};		// Pin para clock de encoder
+	GPIO_Handler_t encodCLK = {0};		// Pin para clock de encoder
+	EXTI_Config_t exti_encodCLK = {0};	// Estructura para EXTI del clock de encoder
 	GPIO_Handler_t encodDT = {0};		// Pin para señal alternativa encoder
-	EXTI_Config_t encodSW = {0}; 		// Pin para botón de cambio de función
+	GPIO_Handler_t encodSW = {0}; 		// Pin para botón de cambio de función
+	EXTI_Config_t exti_encodSW = {0}; 	// Estructura para EXTI de botón switch
 
 
-void init_Display(void);
 void init_Hardware(void);
-
 
 
 
 int main(void){
 
-	init_Display();
 	init_Hardware();
 
 	while(1){
@@ -68,117 +67,108 @@ int main(void){
 }
 
 
-/* Inicialización de los pines a utilizar en el display */
-void init_Display(void){
+void init_Hardware(void){
 
+/* Inicialización de los pines GPIO a utilizar en el display */
 	/* LEDS */
-	handPA8.pGPIOx = GPIOA;
-	handPA8.GPIO_PinConfig.GPIO_PinNumber 		= PIN_8;
-	handPA8.GPIO_PinConfig.GPIO_PinMode 		= GPIO_MODE_OUT;
-	handPA8.GPIO_PinConfig.GPIO_PinOType 		= GPIO_OTYPE_PUSHPULL;
-	handPA8.GPIO_PinConfig.GPIO_PinPuPdControl 	= GPIO_PUPDR_NOTHING;
-	handPA8.GPIO_PinConfig.GPIO_PinAltFunMode  	= AF0;
-	GPIO_Config(&handPA8);
-	GPIO_WritePin(&handPA8, SET);
+	bit0.pGPIOx = GPIOA;
+	bit0.GPIO_PinConfig.GPIO_PinNumber = PIN_8;
+	bit0.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	bit0.GPIO_PinConfig.GPIO_PinOType = GPIO_OTYPE_PUSHPULL;
+	bit0.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	bit0.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
+	GPIO_Config(&bit0);
+	GPIO_WritePin(&bit0, SET);
 
-	handPB10.pGPIOx = GPIOB;
-	handPB10.GPIO_PinConfig.GPIO_PinNumber 			= PIN_10;
-	handPB10.GPIO_PinConfig.GPIO_PinMode 			= GPIO_MODE_OUT;
-	handPB10.GPIO_PinConfig.GPIO_PinOType 			= GPIO_OTYPE_PUSHPULL;
-	handPB10.GPIO_PinConfig.GPIO_PinPuPdControl 	= GPIO_PUPDR_NOTHING;
-	handPB10.GPIO_PinConfig.GPIO_PinAltFunMode  	= AF0;
-	GPIO_Config(&handPB10);
-	GPIO_WritePin(&handPB10, SET);
+	bit1.pGPIOx = GPIOB;
+	bit1.GPIO_PinConfig.GPIO_PinNumber = PIN_10;
+	bit1.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	bit1.GPIO_PinConfig.GPIO_PinOType = GPIO_OTYPE_PUSHPULL;
+	bit1.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	bit1.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
+	GPIO_Config(&bit1);
+	GPIO_WritePin(&bit1, SET);
 
-	handPC4.pGPIOx = GPIOC;
-	handPC4.GPIO_PinConfig.GPIO_PinNumber 		= PIN_4;
-	handPC4.GPIO_PinConfig.GPIO_PinMode 		= GPIO_MODE_OUT;
-	handPC4.GPIO_PinConfig.GPIO_PinOType 		= GPIO_OTYPE_PUSHPULL;
-	handPC4.GPIO_PinConfig.GPIO_PinPuPdControl 	= GPIO_PUPDR_NOTHING;
-	handPC4.GPIO_PinConfig.GPIO_PinAltFunMode  	= AF0;
-	GPIO_Config(&handPC4);
-	GPIO_WritePin(&handPC4, SET);
+	bit2.pGPIOx = GPIOB;
+	bit2.GPIO_PinConfig.GPIO_PinNumber = PIN_6;
+	bit2.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	bit2.GPIO_PinConfig.GPIO_PinOType = GPIO_OTYPE_PUSHPULL;
+	bit2.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	bit2.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
+	GPIO_Config(&bit2);
+	GPIO_WritePin(&bit2, SET);
 
-	handPB5.pGPIOx = GPIOB;
-	handPB5.GPIO_PinConfig.GPIO_PinNumber 		= PIN_5;
-	handPB5.GPIO_PinConfig.GPIO_PinMode 		= GPIO_MODE_OUT;
-	handPB5.GPIO_PinConfig.GPIO_PinOType 		= GPIO_OTYPE_PUSHPULL;
-	handPB5.GPIO_PinConfig.GPIO_PinPuPdControl 	= GPIO_PUPDR_NOTHING;
-	handPB5.GPIO_PinConfig.GPIO_PinAltFunMode  	= AF0;
-	GPIO_Config(&handPB5);
-	GPIO_WritePin(&handPB5, SET);
+	bit3.pGPIOx = GPIOA;
+	bit3.GPIO_PinConfig.GPIO_PinNumber = PIN_7;
+	bit3.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	bit3.GPIO_PinConfig.GPIO_PinOType = GPIO_OTYPE_PUSHPULL;
+	bit3.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+	bit3.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
+	GPIO_Config(&bit3);
+	GPIO_WritePin(&bit3, SET);
 
-	handPB3.pGPIOx = GPIOB;
-	handPB3.GPIO_PinConfig.GPIO_PinNumber 		= PIN_3;
-	handPB3.GPIO_PinConfig.GPIO_PinMode 		= GPIO_MODE_OUT;
-	handPB3.GPIO_PinConfig.GPIO_PinOType 		= GPIO_OTYPE_PUSHPULL;
-	handPB3.GPIO_PinConfig.GPIO_PinPuPdControl 	= GPIO_PUPDR_NOTHING;
-	handPB3.GPIO_PinConfig.GPIO_PinAltFunMode  	= AF0;
-	GPIO_Config(&handPB3);
-	GPIO_WritePin(&handPB3, SET);
+	bit4.pGPIOx = GPIOA;
+	bit4.GPIO_PinConfig.GPIO_PinNumber = PIN_6;
+	bit4.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	bit4.GPIO_PinConfig.GPIO_PinOType = GPIO_OTYPE_PUSHPULL;
+	bit4.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	bit4.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
+	GPIO_Config(&bit4);
+	GPIO_WritePin(&bit4, SET);
 
-	handPA7.pGPIOx = GPIOA;
-	handPA7.GPIO_PinConfig.GPIO_PinNumber 			= PIN_7;
-	handPA7.GPIO_PinConfig.GPIO_PinMode 			= GPIO_MODE_OUT;
-	handPA7.GPIO_PinConfig.GPIO_PinOType 			= GPIO_OTYPE_PUSHPULL;
-	handPA7.GPIO_PinConfig.GPIO_PinPuPdControl 	= GPIO_PUPDR_NOTHING;
-	handPA7.GPIO_PinConfig.GPIO_PinAltFunMode  	= AF0;
-	GPIO_Config(&handPA7);
-	GPIO_WritePin(&handPA7, SET);
+	bit5.pGPIOx = GPIOB;
+	bit5.GPIO_PinConfig.GPIO_PinNumber = PIN_9;
+	bit5.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	bit5.GPIO_PinConfig.GPIO_PinOType = GPIO_OTYPE_PUSHPULL;
+	bit5.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	bit5.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
+	GPIO_Config(&bit5);
+	GPIO_WritePin(&bit5, SET);
 
-	handPA9.pGPIOx = GPIOA;
-	handPA9.GPIO_PinConfig.GPIO_PinNumber 		= PIN_9;
-	handPA9.GPIO_PinConfig.GPIO_PinMode 		= GPIO_MODE_OUT;
-	handPA9.GPIO_PinConfig.GPIO_PinOType 		= GPIO_OTYPE_PUSHPULL;
-	handPA9.GPIO_PinConfig.GPIO_PinPuPdControl 	= GPIO_PUPDR_NOTHING;
-	handPA9.GPIO_PinConfig.GPIO_PinAltFunMode  	= AF0;
-	GPIO_Config(&handPA9);
-	GPIO_WritePin(&handPA9, SET);
+	bit6.pGPIOx = GPIOA;
+	bit6.GPIO_PinConfig.GPIO_PinNumber = PIN_9;
+	bit6.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	bit6.GPIO_PinConfig.GPIO_PinOType = GPIO_OTYPE_PUSHPULL;
+	bit6.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	bit6.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
+	GPIO_Config(&bit6);
+	GPIO_WritePin(&bit6, SET);
 
-	/* Timer */
+	/* Timer para oscilación en transistores */
 	timerDisplay.ptrTIMx = TIM3;
-	timerDisplay.TIMx_Config.TIMx_mode				= BTIMER_MODE_UP;
-	timerDisplay.TIMx_Config.TIMx_period			= 10; // Tiempo en milisegundos
-	timerDisplay.TIMx_Config.TIMx_speed 			= BTIMER_SPEED_1ms;
-	timerDisplay.TIMx_Config.TIMx_interruptEnable 	= 1; // Activar interrupción
+	timerDisplay.TIMx_Config.TIMx_mode = BTIMER_MODE_UP;
+	timerDisplay.TIMx_Config.TIMx_period = 10; // Tiempo en milisegundos
+	timerDisplay.TIMx_Config.TIMx_speed = BTIMER_SPEED_1ms;
+	timerDisplay.TIMx_Config.TIMx_interruptEnable = 1; // Activar interrupción
 	BasicTimer_Config(&timerDisplay);
 
-	/* Transistores */
-	tranUni.pGPIOx = GPIOA;
-	tranUni.GPIO_PinConfig.GPIO_PinNumber 		= PIN_10;
-	tranUni.GPIO_PinConfig.GPIO_PinMode 		= GPIO_MODE_OUT;
-	tranUni.GPIO_PinConfig.GPIO_PinOType 		= GPIO_OTYPE_PUSHPULL;
-	tranUni.GPIO_PinConfig.GPIO_PinPuPdControl 	= GPIO_PUPDR_NOTHING;
-	tranUni.GPIO_PinConfig.GPIO_PinAltFunMode  	= AF0;
+	/* Configuración de pines para transistores */
+	tranUni.pGPIOx = GPIOB;
+	tranUni.GPIO_PinConfig.GPIO_PinNumber = PIN_4;
+	tranUni.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	tranUni.GPIO_PinConfig.GPIO_PinOType = GPIO_OTYPE_PUSHPULL;
+	tranUni.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	tranUni.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
 	GPIO_Config(&tranUni);
 	GPIO_WritePin(&tranUni, SET);
 
-	tranDec.pGPIOx = GPIOA;
-	tranDec.GPIO_PinConfig.GPIO_PinNumber 		= PIN_6;
-	tranDec.GPIO_PinConfig.GPIO_PinMode 		= GPIO_MODE_OUT;
-	tranDec.GPIO_PinConfig.GPIO_PinOType 		= GPIO_OTYPE_PUSHPULL;
-	tranDec.GPIO_PinConfig.GPIO_PinPuPdControl 	= GPIO_PUPDR_NOTHING;
-	tranDec.GPIO_PinConfig.GPIO_PinAltFunMode  	= AF0;
+	tranDec.pGPIOx = GPIOB;
+	tranDec.GPIO_PinConfig.GPIO_PinNumber = PIN_8;
+	tranDec.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	tranDec.GPIO_PinConfig.GPIO_PinOType = GPIO_OTYPE_PUSHPULL;
+	tranDec.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	tranDec.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
 	GPIO_Config(&tranDec);
 	GPIO_WritePin(&tranDec, RESET);
 
-	/* Definicion de posiciones en arreglo para 7 segmentos*/
-	bits_7Seg[0] = handPB3;
-	bits_7Seg[1] = handPC4;
-	bits_7Seg[2] = handPB10;
-	bits_7Seg[3] = handPA8;
-	bits_7Seg[4] = handPA9;
-	bits_7Seg[5] = handPA7;
-	bits_7Seg[6] = handPB5;
-
-	/* Modificacion de variables */
-	decena = numero/10;
-	unidad = numero%10;
-}
-
-
-/* Inicializacion de hardware micro */
-void init_Hardware(void){
+	/* Definicion de posiciones en arreglo para 7 segmentos */
+	bits_7Seg[0] = bit0;
+	bits_7Seg[1] = bit1;
+	bits_7Seg[2] = bit2;
+	bits_7Seg[3] = bit3;
+	bits_7Seg[4] = bit4;
+	bits_7Seg[5] = bit5;
+	bits_7Seg[6] = bit6;
 
 	// Timer para LED de estado usando el LED2
 	timerLED2.ptrTIMx = TIM2;
@@ -188,7 +178,7 @@ void init_Hardware(void){
 	timerLED2.TIMx_Config.TIMx_interruptEnable = 1; // Activar interrupción
 	BasicTimer_Config(&timerLED2);
 
-	// Controlador de LED2 asignado para el estado de la función
+	// Controlador de LED2 asignado como led de estado
 	handLED2.pGPIOx = GPIOA;
 	handLED2.GPIO_PinConfig.GPIO_PinNumber = PIN_5;
 	handLED2.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
@@ -196,37 +186,41 @@ void init_Hardware(void){
 	handLED2.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
 	handLED2.GPIO_PinConfig.GPIO_PinAltFunMode = AF0;
 	GPIO_Config(&handLED2);
-}
 
 
 /* Inicialización de los pines asociados a las señales entregadas
  * por el encoder */
-void init_Encoder(void){
 
-	// Señal del encoder que dirigirá la interrupción EXTI11
-	encodCLK.edgeType = EXTERNAL_INTERRUPT_FALLING_EDGE;
-	encodCLK.pGPIOHandler->pGPIOx = GPIOB;
-	encodCLK.pGPIOHandler->GPIO_PinConfig.GPIO_PinNumber = PIN_2;
-	encodCLK.pGPIOHandler->GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
-	encodCLK.pGPIOHandler->GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
-	extInt_Config(&encodCLK);
+		// GPIO para clock de encoder
+		encodCLK.pGPIOx = GPIOB;
+		encodCLK.GPIO_PinConfig.GPIO_PinNumber = PIN_5;
+		encodCLK.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+		encodCLK.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
 
-	// Señal del encoder que determinará la dirección del movimiento
-	encodDT.pGPIOx = GPIOB;
-	encodDT.GPIO_PinConfig.GPIO_PinNumber = PIN_1;
-	encodDT.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
-	encodDT.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
-	GPIO_Config(&encodDT);
+		// EXTI para interrupción realizada por el clock del encoder
+		exti_encodCLK.edgeType = EXTERNAL_INTERRUPT_FALLING_EDGE;
+		exti_encodCLK.pGPIOHandler = &encodCLK;
+		extInt_Config(&exti_encodCLK);
 
-	// Señal de encoder que lidera la interrupción EXTI12 de cambio de función
-	encodSW.edgeType = EXTERNAL_INTERRUPT_FALLING_EDGE;
-	encodSW.pGPIOHandler->pGPIOx = GPIOB;
-	encodSW.pGPIOHandler->GPIO_PinConfig.GPIO_PinNumber = PIN_15;
-	encodSW.pGPIOHandler->GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
-	encodSW.pGPIOHandler->GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
-	extInt_Config(&encodSW);
+		// GPIO para señal DT del encoder que determinará la dirección del movimiento
+		encodDT.pGPIOx = GPIOB;
+		encodDT.GPIO_PinConfig.GPIO_PinNumber = PIN_3;
+		encodDT.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+		encodDT.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+		GPIO_Config(&encodDT);
 
+		// GPIO para botón del encoder que determinará cambio de función
+		encodSW.pGPIOx = GPIOA;
+		encodSW.GPIO_PinConfig.GPIO_PinNumber = PIN_10;
+		encodSW.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+		encodSW.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+
+		// EXTI para interrupción realizada por el botón del encoder
+//		exti_encodSW.edgeType = EXTERNAL_INTERRUPT_FALLING_EDGE;
+//		exti_encodCLK.pGPIOHandler = &encodSW;
+//		extInt_Config(&exti_encodSW);
 }
+
 
 
 /* Centelleo de LED de estado */
@@ -254,15 +248,16 @@ void BasicTimer3_Callback(void){
 
 
 /* Incremento o decremento por encoder */
-void callback_extInt2(void){
-	giro_Numero(numero, encodDT);
+void callback_extInt5(void){
+	modo++;
+//	giro_Numero(numero, encodDT);
 }
 
 
 /* Modo de funcion en programa*/
-void callback_extInt15(void){
-	cambio_modo(modo);
-}
+//void callback_extInt10(void){
+//	cambio_modo(modo);
+//}
 
 
 
